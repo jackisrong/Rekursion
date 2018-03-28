ca515c2072f9a59d23472d13e4e661ca1e04a4c5
323984955 <323984955@C-3Z02Y4J.tdsb.on.ca>
2018-03-28 08:59:19 -0400

    Methods added.

--- src/recursion/Recursion.java
+++ src/recursion/Recursion.java
@@ -1,9 +1,89 @@
 package recursion;
 
+import java.util.LinkedList;
+
 public class Recursion {
 
-    public static void main(String[] args) {
-	// TODO code application logic here
+    // THE FOLLOWING ARE NOT RECURSIVE
+    private static LinkedList add(String value, LinkedList list) {
+	// Add value to the front of list. If list is null, create a new LinkedList.
+
+	return null;
     }
+
+    private static String first(LinkedList list) {
+	// Return the first element (String) in list
+
+	return null;
+    }
+
+    private static LinkedList rest(LinkedList list) {
+	// Return all the elements except the first in the list
+
+	return null;
+    }
+
+    private static boolean isNull(LinkedList list) {
+	// Determine if list is null
+
+	return true;
+    }
+
+    private static boolean isEqual(String s1, String s2) {
+	// Determine if two Strings are equal
+
+	return true;
+    }
+
+    private static void printList(LinkedList list) {
+	// Print each item in list on a new line
+    }
+
+    private static LinkedList append(LinkedList list1, LinkedList list2) {
+	if (isNull(list1)) {
+	    return list2;
+	} else {
+	    return add(first(list1), append(rest(list1), list2));
+	}
+    }
+
     
+    
+    
+        
+    // THE FOLLOWING MUST BE RECURSIVE
+    private static boolean isMember(String member, LinkedList list) {
+	// Returns true if member is an item in the list.
+
+	return true;
+    }
+
+    private static LinkedList deleteFirst(String delete, LinkedList list) {
+	// Returns a list in the same order, but with delete missing.
+	//If there is nothing to delete, return the original list.
+
+	return null;
+    }
+
+    private static LinkedList deleteAll(String delete, LinkedList list) {
+	// Returns a list in the same order, but with all occurrences of delete missing.
+	//If there is nothing to delete, return the original list.
+
+	return null;
+    }
+
+    private static LinkedList reverse(LinkedList list) {
+	// Returns the reversed version of list.
+
+	return null;
+    }
+
+    
+    
+    
+    
+    public static void main(String[] args) {
+	printList(append(add("Tic", add("Tac", add("Toe", null))), add("Toe", add("Tic", add("Tac", null)))));
+    }
+
 }
