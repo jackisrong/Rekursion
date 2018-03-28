f194c5444c0c3671c831e9354214cd1981c895c1
323984955 <323984955@C-3Z02Y4J.tdsb.on.ca>
2018-03-28 09:07:33 -0400

    Doubly link list classes added.

---
+++ src/recursion/MyDLL.java
@@ -0,0 +1,41 @@
+package recursion;
+
+public class MyDLL {
+
+    public void addNode(Integer value) {
+	// Inserts node to the end of the linked list
+    }
+
+    public void deleteNode(Integer value) {
+	// Deletes all nodes with the given value
+    }
+
+    public void sort() {
+	// Sorts the linked list in ascending order
+    }
+
+    public void reverse() {
+	// Reverse the nodes in the linked list.
+    }
+
+    public void duplicate() {
+	// Removes all duplicate nodes in the linked list
+    }
+
+    public void alternate(MyDLL list) {
+	// Merges the contents of list and this class in alternating order class = 1 2 3,
+	// list = 4 4 4  alternate = 1 4 2 4 3 4
+    }
+
+    public void optiFind(Integer value) {
+	// Searches for value and places it at the front of the doubly linked list
+    }
+
+    public void middleAdd(Integer value) {
+	// Places the new node in the middle of the list
+    }
+
+    public void printList() {
+	// Prints the contents of the linked list
+    }
+}
