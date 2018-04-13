2e0c8ed0f39b88a73ce09e4074be1b8d817aa013
kristatian <kristatian@10.2.97.182>
2018-04-13 19:46:39 -0400

    fixed and added some methods in MyDLL and Recursion class

--- src/recursion/MyDLL.java
+++ src/recursion/MyDLL.java
@@ -6,97 +6,121 @@ public class MyDLL {
     private DLLNode tail;
 
     public MyDLL() {
-	this.head = null;
-	this.tail = null;
+        this.head = null;
+        this.tail = null;
     }
 
-    // NONE OF THESE METHODS HAVE BEEN TESTED
     public void addNode(Integer value) {
-	// Inserts node to the end of the linked list
-	DLLNode node = new DLLNode(value);
-	DLLNode tail = this.tail;
-	this.tail.setNext(node);
-	node.setPrevious(tail);
-	this.tail = node;
+        // Inserts node to the end of the linked list
+        DLLNode node = new DLLNode(value);
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            this.tail.setNext(node);
+            node.setPrevious(this.tail);
+            this.tail = node;
+        }
     }
 
     public void deleteNode(Integer value) {
-	// Deletes all nodes with the given value
-	DLLNode listHead = head;
-	while (listHead != null) {
-	    if (listHead.getValue() == value) {
-		listHead.getPrevious().setNext(listHead.getNext());
-		listHead.getNext().setPrevious(listHead.getPrevious());
-	    }
-	    listHead = listHead.getNext();
-	}
+        // Deletes all nodes with the given value
+        DLLNode listHead = head;
+        while (listHead != null) {
+            if (listHead.getValue().equals(value)) {
+                listHead.getPrevious().setNext(listHead.getNext());
+                //NULLPOINTER OCCURS WHEN LIST.GETNEXT() IS NULL AT END OF LIST
+                listHead.getNext().setPrevious(listHead.getPrevious());
+            }
+            listHead = listHead.getNext();
+        }
     }
 
     public void sort() {
-	// Sorts the linked list in ascending order
+        // Sorts the linked list in ascending order
+        if (head != null) {
+            DLLNode swapNode = head;
+            while (swapNode != null) {
+                DLLNode current = swapNode;
+                int smallest = swapNode.getValue();
+                DLLNode smallestNode = swapNode;
+                while (current != null) {
+                    if (current.getValue() < smallest) {
+                        smallest = current.getValue();
+                        smallestNode = current;
+                    }
+                    current = current.getNext();
+                }
+                smallestNode.value = swapNode.getValue();
+                swapNode.value = smallest; 
+                swapNode = swapNode.getNext();
+            }
+        }
     }
 
     public void reverse() {
-	// Reverse the nodes in the linked list.
-	MyDLL tempList = new MyDLL();
+        // Reverse the nodes in the linked list.
+        MyDLL tempList = new MyDLL();
     }
 
     public void duplicate() {
-	// Removes all duplicate nodes in the linked list
+        // Removes all duplicate nodes in the linked list
     }
 
     public void alternate(MyDLL list) {
-	// Merges the contents of list and this class in alternating order class = 1 2 3,
-	// list = 4 4 4  alternate = 1 4 2 4 3 4
+        // Merges the contents of list and this class in alternating order class = 1 2 3,
+        // list = 4 4 4  alternate = 1 4 2 4 3 4
 
-	DLLNode classHead = this.head;
-	DLLNode listHead = list.head;
+        DLLNode classHead = this.head;
+        DLLNode listHead = list.head;
+        
+        //THIS LOOP ISN'T DONE; NOTHING ACTUALLY HAPPENS
+        //LIST IS PRINTED BUT LIKE, THAT'S IT LOL
+        while (listHead != null) {
+            System.out.print(classHead.getValue() + listHead.getValue());
+            classHead = classHead.getNext();
+            listHead = listHead.getNext();
+        }
 
-	while (listHead != null) {
-	    System.out.print(classHead.getValue() + listHead.getValue());
-	    classHead = classHead.getNext();
-	    listHead = listHead.getNext();
-	}
-
-	System.out.println();
+        System.out.println();
     }
 
     public void optiFind(Integer value) {
-	// Searches for value and places it at the front of the doubly linked list
+        // Searches for value and places it at the front of the doubly linked list
     }
 
     public void middleAdd(Integer value) {
-	// Places the new node in the middle of the list
-	int listLength = 0;
-	DLLNode listHead = head;
+        // Places the new node in the middle of the list
+        int listLength = 0;
+        DLLNode listHead = head;
 
-	while (listHead != null) {
-	    listLength++;
-	    listHead = listHead.getNext();
-	}
+        while (listHead != null) {
+            listLength++;
+            listHead = listHead.getNext();
+        }
 
-	DLLNode before = head;
-	DLLNode after = head.getNext();
-	for (int i = 0; i < listLength; i++) {
-	    if (i == Math.round(listLength / 2)) {
-		DLLNode node = new DLLNode(value);
-		node.setPrevious(before);
-		before.setNext(node);
-		node.setNext(after);
-		after.setPrevious(node);
-	    }
-	    before = before.getNext();
-	    after = after.getNext();
-	}
+        DLLNode before = head;
+        DLLNode after = head.getNext();
+        for (int i = 0; i < listLength - 1; i++) {
+            if (i == Math.round(listLength / 2)) {
+                DLLNode node = new DLLNode(value);
+                node.setPrevious(before);
+                before.setNext(node);
+                node.setNext(after);
+                after.setPrevious(node);
+            }
+            before = before.getNext();
+            after = after.getNext();
+        }
     }
 
     public void printList() {
-	// Prints the contents of the linked list
-	DLLNode after = head;
+        // Prints the contents of the linked list
+        DLLNode after = head;
 
-	while (after != null) {
-	    System.out.println(after.getValue());
-	    after = after.getNext();
-	}
+        while (after != null) {
+            System.out.println(after.getValue());
+            after = after.getNext();
+        }
     }
 }
--- src/recursion/Recursion.java
+++ src/recursion/Recursion.java
@@ -6,237 +6,255 @@ public class Recursion {
     // THE FOLLOWING
     // ARE NOT RECURSIVE
     private static LinkedList add(String value, LinkedList list) {
-	// Add value to the front of list. If list is null, create a new LinkedList.
-	if (list == null || list.getHead() == null) {
-	    LinkedList newList = new LinkedList();
-	    Node node = new Node(value);
-	    newList.setHead(node);
-	    return newList;
-	} else {
-	    LinkedList newList = rest(list);
-	    String previousHeadValue = list.getHead().getValue();
-	    Node newHead = new Node(value);
-	    Node previousHead = new Node(previousHeadValue);
-	    previousHead.setNext(newList.getHead());
-	    newHead.setNext(previousHead);
-	    newList.setHead(newHead);
-	    return newList;
-	}
+        // Add value to the front of list. If list is null, create a new LinkedList.
+        if (list == null || list.getHead() == null) {
+            LinkedList newList = new LinkedList();
+            Node node = new Node(value);
+            newList.setHead(node);
+            return newList;
+        } else {
+            LinkedList newList = rest(list);
+            String previousHeadValue = list.getHead().getValue();
+            Node newHead = new Node(value);
+            Node previousHead = new Node(previousHeadValue);
+            previousHead.setNext(newList.getHead());
+            newHead.setNext(previousHead);
+            newList.setHead(newHead);
+            return newList;
+        }
     }
 
     private static String first(LinkedList list) {
-	// Return the first element (String) in list
-	if (!isNull(list)) {
-	    return list.getHead().getValue();
-	}
-	
-	return null;
+        // Return the first element (String) in list
+        if (!isNull(list)) {
+            return list.getHead().getValue();
+        }
+
+        return null;
     }
 
     private static LinkedList rest(LinkedList list) {
-	// Return all the elements except the first in the list
-	if (isNull(list)) {
-	    return null;
-	}
+        // Return all the elements except the first in the list
+        if (isNull(list)) {
+            return null;
+        }
 
-	Node secondNode = list.getHead().getNext();
-	LinkedList newLinkedList = new LinkedList();
+        Node secondNode = list.getHead().getNext();
+        LinkedList newLinkedList = new LinkedList();
 
-	while (secondNode != null) {
-	    if (newLinkedList.getHead() == null) {
-		newLinkedList.setHead(new Node(secondNode.getValue()));
-	    } else {
-		Node currentNode = newLinkedList.getHead();
-		while (currentNode.getNext() != null) {
-		    currentNode = currentNode.getNext();
-		}
-		currentNode.setNext(new Node(secondNode.getValue()));
-	    }
-	    secondNode = secondNode.getNext();
-	}
+        while (secondNode != null) {
+            if (newLinkedList.getHead() == null) {
+                newLinkedList.setHead(new Node(secondNode.getValue()));
+            } else {
+                Node currentNode = newLinkedList.getHead();
+                while (currentNode.getNext() != null) {
+                    currentNode = currentNode.getNext();
+                }
+                currentNode.setNext(new Node(secondNode.getValue()));
+            }
+            secondNode = secondNode.getNext();
+        }
 
-	return newLinkedList;
+        return newLinkedList;
     }
 
     private static boolean isNull(LinkedList list) {
-	// Determine if list is null
-	return list == null || list.getHead() == null;
+        // Determine if list is null
+        return list == null || list.getHead() == null;
     }
 
     private static boolean isEqual(String s1, String s2) {
-	// Determine if two Strings are equal
-	return s1.equals(s2);
+        // Determine if two Strings are equal
+        return s1.equals(s2);
     }
 
     private static void printList(LinkedList list) {
-	// Print each item in list on a new line
-	Node after = list.getHead();
+        // Print each item in list on a new line
+        Node after = list.getHead();
 
-	while (after != null) {
-	    System.out.println(after.getValue());
-	    after = after.getNext();
-	}
+        while (after != null) {
+            System.out.println(after.getValue());
+            after = after.getNext();
+        }
     }
 
     private static LinkedList append(LinkedList list1, LinkedList list2) {
-	if (isNull(list1)) {
-	    return list2;
-	} else {
-	    return add(first(list1), append(rest(list1), list2));
-	}
+        if (isNull(list1)) {
+            return list2;
+        } else {
+            return add(first(list1), append(rest(list1), list2));
+        }
     }
 
     // THE FOLLOWING
     // MUST BE RECURSIVE
     private static boolean isMember(String member, LinkedList list) {
-	// Returns true if member is an item in the list.
-	if (isNull(list)) {
-	    return false;
-	} else if (isEqual(member, first(list))) {
-	    return true;
-	}
+        // Returns true if member is an item in the list.
+        if (isNull(list)) {
+            return false;
+        } else if (isEqual(member, first(list))) {
+            return true;
+        }
 
-	return isMember(member, rest(list));
+        return isMember(member, rest(list));
     }
 
     private static LinkedList deleteFirst(String delete, LinkedList list) {
-	// Returns a list in the same order, but with delete missing.
-	// If there is nothing to delete, return the original list.
-	if (!isMember(delete, list)) {
-	    return list;
-	} else if (first(list).equals(delete)) {
-	    return rest(list);
-	}
+        // Returns a list in the same order, but with delete missing.
+        // If there is nothing to delete, return the original list.
+        if (!isMember(delete, list)) {
+            return list;
+        } else if (first(list).equals(delete)) {
+            return rest(list);
+        }
 
-	return append(add(first(list), null), deleteFirst(delete, rest(list)));
+        return append(add(first(list), null), deleteFirst(delete, rest(list)));
     }
 
     private static LinkedList deleteAll(String delete, LinkedList list) {
-	// Returns a list in the same order, but with all occurrences of delete missing.
-	// If there is nothing to delete, return the original list.
-	if (!isMember(delete, list)) {
-	    return list;
-	} else if (first(list).equals(delete)) {
-	    return deleteFirst(delete, rest(list));
-	}
+        // Returns a list in the same order, but with all occurrences of delete missing.
+        // If there is nothing to delete, return the original list.
+        if (!isMember(delete, list)) {
+            return list;
+        } else if (first(list).equals(delete)) {
+            return deleteFirst(delete, rest(list));
+        }
 
-	return append(add(first(list), null), deleteAll(delete, rest(list)));
+        return append(add(first(list), null), deleteAll(delete, rest(list)));
     }
 
     private static LinkedList reverse(LinkedList list) {
-	// Returns the reversed version of list.
-	if (isNull(list)) {
-	    return list;
-	} else if (list.getHead().getNext() == null) {
-	    return list;
-	}
+        // Returns the reversed version of list.
+        if (isNull(list)) {
+            return list;
+        } else if (list.getHead().getNext() == null) {
+            return list;
+        }
 
-	return append(reverse(rest(list)), add(first(list), null));
+        return append(reverse(rest(list)), add(first(list), null));
     }
 
     // THE FOLLOWING MUST BE RECURSIVE, ASSUME THE USER WILL NEVER INPUT A BLANK LIST,
     // THERE'RE NO REPEATED ELEMENTS, AND THE ORDER OF THE RETURNED LIST DOESN'T MATTER.
     private static LinkedList difference(LinkedList list1, LinkedList list2) {
-	// Returns a list of the difference between list1 and list2
+        // Returns a list of the difference between list1 and list2
 
-	// I THINK DIFFERENCE MEANS ALL THE ELEMENTS IN LIST1 THAT AREN'T IN LIST2 BUT I'M NOT SURE
-	if (isNull(list2)) {
-	    return list1;
-	} else if (isMember(list2.getHead().getValue(), list2)) {
-	    list1 = deleteAll(list2.getHead().getValue(), list1);
-	}
+        // I THINK DIFFERENCE MEANS ALL THE ELEMENTS IN LIST1 THAT AREN'T IN LIST2 BUT I'M NOT SURE
+        if (isNull(list2)) {
+            return list1;
+        } else if (isMember(list2.getHead().getValue(), list2)) {
+            list1 = deleteAll(list2.getHead().getValue(), list1);
+        }
 
-	return difference(list1, rest(list2));
+        return difference(list1, rest(list2));
     }
 
     private static LinkedList union(LinkedList list1, LinkedList list2) {
-	// Returns a list of the union between list1 and list2.
+        // Returns a list of the union between list1 and list2.
 
-	// I THINK UNION IS BOTH LISTS TOGETHER, EXCEPT DUPLICATES ONLY APPEAR ONCE
-	if (isNull(list2)) {
-	    return list1;
-	} else if (!isMember(list2.getHead().getValue(), list1)) {
-	    list1 = add(list2.getHead().getValue(), list1);
-	}
+        // I THINK UNION IS BOTH LISTS TOGETHER, EXCEPT DUPLICATES ONLY APPEAR ONCE
+        if (isNull(list2)) {
+            return list1;
+        } else if (!isMember(list2.getHead().getValue(), list1)) {
+            list1 = add(list2.getHead().getValue(), list1);
+        }
 
-	return union(list1, rest(list2));
+        return union(list1, rest(list2));
     }
 
     private static LinkedList intersect(LinkedList list1, LinkedList list2) {
-	// Returns a list of the intersection between list1 and list2.
+        // Returns a list of the intersection between list1 and list2.
+        if (isNull(list2)) {
+            return null;
+        } else if (isMember(list2.getHead().getValue(), list1)) {
+            return add(list2.getHead().getValue(), intersect(list1, rest(list2)));
+        }
 
-	// I THINK INTERSECT IS THE MIDDLE PART OF A VEN DIAGRAM
-	if (isNull(list2)) {
-	    return list1;
-	} else if (!isMember(list2.getHead().getValue(), list1)) {
-	    // THIS DOESN'T FUCKING WORK, IT SHOULD RETURN 2 AND 4
-	}
-
-	return intersect(list1, rest(list2));
+        return intersect(list1, rest(list2));
     }
 
     private static boolean subset(LinkedList list1, LinkedList list2) {
-	// Returns true if list1 is a subset of list2. Otherwise it returns false.
+        // Returns true if list1 is a subset of list2. Otherwise it returns false.
 
-	// I THINK THIS WOULD CHECK IF ALL ELEMENTS IN LIST1 BELONG TO LIST2
-	if (isNull(list1)) {
-	    return true;
-	} else if (!isMember(list1.getHead().getValue(), list2)) {
-	    return false;
-	}
+        // I THINK THIS WOULD CHECK IF ALL ELEMENTS IN LIST1 BELONG TO LIST2
+        if (isNull(list1)) {
+            return true;
+        } else if (!isMember(list1.getHead().getValue(), list2)) {
+            return false;
+        }
 
-	return subset(rest(list1), list2);
+        return subset(rest(list1), list2);
     }
 
     private static boolean equalSets(LinkedList list1, LinkedList list2) {
-	// Returns true if list1 and list2 are equal, but not necessarily in the same order.
-	// Otherwise it returns false.
+        // Returns true if list1 and list2 are equal, but not necessarily in the same order.
+        // Otherwise it returns false.
 
-	if (isNull(list2)) {
-	    return true;
-	} else if (!isMember(list2.getHead().getValue(), list1)) {
-	    return false;
-	    // I'M NOT 100% SURE IF THIS METHOD WORKS YET
-	    // ACTUALLY I'M NOT SURE IF ANY OF THE METHODS WORK YET
-	}
-
-	return equalSets(list1, rest(list2));
+        if (isNull(list2)) {
+            return true;
+        } else if (!isMember(list2.getHead().getValue(), list1)) {
+            return false;
+        }
+        return equalSets(list1, rest(list2));
     }
 
     public static void main(String[] args) {
-	System.out.println("Testing append():");
-	printList(append(add("Tic", add("Tac", add("Toe", null))), add("Toe", add("Tic", add("Tac", null)))));
+        System.out.println("Testing append():");
+        printList(append(add("Tic", add("Tac", add("Toe", null))), add("Toe", add("Tic", add("Tac", null)))));
 
-	System.out.println("\nTesting deleteFirst() with LinkedList with multiple Toe occurences:");
-	printList(deleteFirst("Toe", append(add("Tic", add("Tac", add("Toe", null))), add("Toe", add("Tic", add("Tac", null))))));
+        System.out.println("\nTesting deleteFirst() with LinkedList with multiple Toe occurences:");
+        printList(deleteFirst("Toe", append(add("Tic", add("Tac", add("Toe", null))), add("Toe", add("Tic", add("Tac", null))))));
 
-	System.out.println("\nTesting deleteFirst() with LinkedList with single Toe occurences:");
-	printList(deleteFirst("Toe", add("Tic", add("Tac", add("Toe", null)))));
+        System.out.println("\nTesting deleteFirst() with LinkedList with single Toe occurences:");
+        printList(deleteFirst("Toe", add("Tic", add("Tac", add("Toe", null)))));
 
-	System.out.println("\nTesting deleteAll() with LinkedList with multiple Tic occurences:");
-	printList(deleteAll("Tic", append(add("Tic", add("Tac", add("Toe", null))), add("Toe", add("Tic", add("Tac", null))))));
+        System.out.println("\nTesting deleteAll() with LinkedList with multiple Tic occurences:");
+        printList(deleteAll("Tic", append(add("Tic", add("Tac", add("Toe", null))), add("Toe", add("Tic", add("Tac", null))))));
 
-	System.out.println("\nTesting deleteAll() with LinkedList with single Tic occurences:");
-	printList(deleteAll("Tic", add("Tic", add("Tac", add("Toe", null)))));
+        System.out.println("\nTesting deleteAll() with LinkedList with single Tic occurences:");
+        printList(deleteAll("Tic", add("Tic", add("Tac", add("Toe", null)))));
 
-	System.out.println("\nTesting reverse():");
-	printList(reverse(add("Tic", add("Tac", add("Toe", null)))));
+        System.out.println("\nTesting reverse():");
+        printList(reverse(add("Tic", add("Tac", add("Toe", null)))));
 
-	System.out.println("\nTesting difference():");
-	printList(difference(add("2", add("4", add("6", add("8", add("10", null))))), add("1", add("2", add("3", add("4", add("5", null)))))));
+        System.out.println("\nTesting difference():");
+        printList(difference(add("2", add("4", add("6", add("8", add("10", null))))), add("1", add("2", add("3", add("4", add("5", null)))))));
 
-	System.out.println("\nTesting union():");
-	printList(union(add("2", add("4", add("6", add("8", add("10", null))))), add("1", add("2", add("3", add("4", add("5", null)))))));
+        System.out.println("\nTesting union():");
+        printList(union(add("2", add("4", add("6", add("8", add("10", null))))), add("1", add("2", add("3", add("4", add("5", null)))))));
 
-	System.out.println("\nTesting intersect():");
-	printList(intersect(add("2", add("4", add("6", add("8", add("10", null))))), add("1", add("2", add("3", add("4", add("5", null)))))));
+        System.out.println("\nTesting intersect():");
+        printList(intersect(add("2", add("4", add("6", add("8", add("10", null))))), add("1", add("2", add("3", add("4", add("5", null)))))));
 
-	System.out.println("\nTesting subset():");
-	System.out.println(subset(add("1", add("2", add("3", null))), add("1", add("2", add("3", add("4", add("5", null)))))));
-	System.out.println(subset(add("1", add("2", add("6", null))), add("1", add("2", add("3", add("4", add("5", null)))))));
+        System.out.println("\nTesting subset():");
+        System.out.println(subset(add("1", add("2", add("3", null))), add("1", add("2", add("3", add("4", add("5", null)))))));
+        System.out.println(subset(add("1", add("2", add("6", null))), add("1", add("2", add("3", add("4", add("5", null)))))));
 
-	System.out.println("\nTesting equalSets():");
-	System.out.println(equalSets(add("1", add("2", add("3", add("4", add("5", null))))), add("1", add("2", add("3", add("4", add("5", null)))))));
-	System.out.println(equalSets(add("1", add("2", add("6", null))), add("1", add("2", add("3", add("4", add("5", null)))))));
+        System.out.println("\nTesting equalSets():");
+        System.out.println(equalSets(add("1", add("2", add("3", add("5", add("4", null))))), add("3", add("2", add("1", add("4", add("5", null)))))));
+        System.out.println(equalSets(add("1", add("2", add("6", null))), add("1", add("2", add("3", add("4", add("5", null)))))));
+        
+        MyDLL dll = new MyDLL();
+        dll.addNode(12);
+        dll.addNode(5);
+        dll.addNode(0);
+        dll.addNode(55);
+        dll.printList();
+        dll.sort();
+        dll.printList();
+        System.out.println("------");
+//        dll.deleteNode(55);
+//        dll.printList();
+        dll.middleAdd(500);
+        System.out.println("middle add 500");
+        dll.printList();
+        MyDLL dll2 = new MyDLL();
+        dll2.addNode(-1);
+        dll2.addNode(-5);
+        dll2.addNode(-33);
+        dll2.addNode(-15);
+        dll.alternate(dll2);
+        System.out.println("alternate");
+        dll.printList();
     }
 }
